2a8f6a970d4349234e24d97f9db84f8b411de271
Dhananjay Mahajan <dhananjaymahajan2001@gmail.com>
2022-11-02 05:52:52 +0000

    Update 250 files

--- src/index.js
+++ src/index.js
@@ -1,22 +1,35 @@
-import "./styles.css";
-
-function validatePalin(str) {
-  // get the total length of the words
-  const len = string.length;
-
-  // Use for loop to divide the words into 2 half
-  for (let i = 0; i < len / 2; i++) {
-    // validate the first and last characters are same
-    if (string[i] !== string[len - 1 - i]) {
-      alert("It is not a palindrome");
+function check_palindrome(str) {
+  let j = str.length - 1;
+  for (let i = 0; i < j / 2; i++) {
+    let x = str[i]; //forward character
+    let y = str[j - i]; //backward character
+    if (x != y) {
+      // return false if string not match
+      return false;
     }
   }
-  alert("It is a palindrome");
+  /// return true if string is palindrome
+  return true;
 }
 
-// accept the string or number from the prompt
-const string = prompt("Enter a string or number: ");
-
-const value = validatePalin(string);
-
-console.log(value);
+//function that print output is string is palindrome
+function is_palindrome(str) {
+  // variable that is true if string is palindrome
+  let ans = check_palindrome(str);
+  //condition checking ans is true or not
+  if (ans == true) {
+    console.log("passed string is palindrome ");
+  } else {
+    console.log("passed string not a palindrome");
+  }
+}
+//take input from terminal
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+readline.question("Enter string : ", (name) => {
+  let test = name;
+  is_palindrome(test);
+  readline.close();
+});
